7427fca9c70b282c8246eb95d00473eae1537ad8
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2024-10-16 11:57:36 +0530

    Fix quantity validator error placement

--- src/components/AddToCart.vue
+++ src/components/AddToCart.vue
@@ -39,26 +39,28 @@ watch(quantity, () => {
 });
 
 </script>
-<template> 
-  <div class="quantity cart-quantity">
-    <button class="quantity__button" name="minus" type="button" v-on:click="decrementQuantity">
-      <!-- <span class="visually-hidden">
+<template>
+  <div class="card__information row-auto">
+    <div class="quantity cart-quantity">
+      <button class="quantity__button" name="minus" type="button" v-on:click="decrementQuantity">
+        <!-- <span class="visually-hidden">
       {{- 'products.product.quantity.decrease' | t: product: variant.title | escape -}}
     </span> -->
-      <div v-html="SVG_CONSTANTS.MINUS"></div>
-    </button>
-    <input class="quantity__input" data-quantity-variant-id="{{ variant.id }}" type="number"
-      name="updates[{{ variant_id }}]" v-model="quantity" min="props.min" max="props.max"
-      aria-label="{{ 'products.product.quantity.input_label' | t: product: variant.title | escape }}"
-      id="Quantity-{{ variant.id }}" data-index="{{ variant.id }}" />
-    <button class="quantity__button" name="plus" type="button" v-on:click="incrementQuantity">
+        <div v-html="SVG_CONSTANTS.MINUS"></div>
+      </button>
+      <input class="quantity__input" data-quantity-variant-id="{{ variant.id }}" type="number"
+        name="updates[{{ variant_id }}]" v-model="quantity" min="props.min" max="props.max"
+        aria-label="{{ 'products.product.quantity.input_label' | t: product: variant.title | escape }}"
+        id="Quantity-{{ variant.id }}" data-index="{{ variant.id }}" />
+      <button class="quantity__button" name="plus" type="button" v-on:click="incrementQuantity">
 
-      <div v-html="SVG_CONSTANTS.PLUS"></div>
-    </button>
-    <!-- {%- render 'progress-bar' -%} -->
-  </div>
-  <div class="quantity-validation caption-with-letter-spacing" v-if="quantityError">
-    {{ quantityError }}
+        <div v-html="SVG_CONSTANTS.PLUS"></div>
+      </button>
+      <!-- {%- render 'progress-bar' -%} -->
+    </div>
+    <div class="quantity-validation caption-with-letter-spacing row-auto" v-if="quantityError">
+      {{ quantityError }}
+    </div>
   </div>
 </template>
 
@@ -66,6 +68,7 @@ watch(quantity, () => {
 .quantity {
   width: auto;
 }
+
 .quantity-validation {
   color: red;
 }
