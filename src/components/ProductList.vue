9c9e7128d03f8e94f982cbe93e3ec91578fb8650
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2024-11-04 11:56:46 +0530

    Working on product list page

---
+++ src/components/ProductList.vue
@@ -0,0 +1,22 @@
+<script>
+import { ref } from 'vue';
+// import compareDate from '@/scripts/customFilters/filters/compareDate';
+import AddToCart from './AddToCart.vue';
+export default {
+
+  components: {
+    AddToCart
+  },
+
+  setup() {
+
+    const greeting = ref("test")
+    const testfn = () =>console.log("button click", greeting);
+    return {
+      greeting,
+      testfn,
+      // compareDate
+    }
+  }
+}
+</script>
