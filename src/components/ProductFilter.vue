113d5cd06b9d6645bb46d403dd058f5e5be915da
AbhishekKadam11 <abhishek.kadam007@gmail.com>
2024-11-23 20:48:29 +0530

    Created filter object in component

--- src/components/ProductFilter.vue
+++ src/components/ProductFilter.vue
@@ -10,26 +10,34 @@ export default {
   setup() {
 
     const rootElement = ref(null);
-    let processorType = ref(null);
-    const prductFilters = ref(null);
+    const prductFilters = ref([]);
 
     onMounted((data, target) => {
       if (rootElement.value) {
         try {
           let filterObj = rootElement.value.dataset.productInfo.substring(0, rootElement.value.dataset.productInfo.length - 1);
           console.log("product info:",filterObj )
-          // processorType.value = JSON.parse(rootElement.value.dataset.productTags).filter(el =>  el.split("processor_type:")[1]);
-          // console.log('product in vue', processorType);
+          filterObj.split('start=').map((e) => {
+            let item = {};
+            e.split(',').map((it) => {
+              if (it) {
+                let keyPair = it?.split(':');
+                item[keyPair[0]] = keyPair[1];
+              }
+            })
+            Object.keys(item).length > 0 ? prductFilters.value.push(item) : null;
+          })
+          console.log("prductFilters =>",  prductFilters);
         } catch (e) {
+          console.log("prductFilters e=>", e);
           throw new Error("Unable to process product tags", e);
         }
       }
     });
 
     return {
-      // compareDate
       rootElement,
-      processorType
+      prductFilters,
     }
   }
 }
